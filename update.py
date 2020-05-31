7a10b55549aa6ecc8063f654a96c477f1ea23138
Igor Sednev <sednev@raveinfosys.com>
2020-05-31 18:18:00 +0300

    chane Magento version

--- update.py
+++ update.py
@@ -20,7 +20,7 @@ files_to_copy = ("m2test.py", "deploypipeline.py", "entrypoint.sh", "hooks", "as
 
 magento_versions = {
     "test": {
-        "exact_version": "2.3.3",
+        "exact_version": "2.3.4",
         "php_versions": ["7.4", "7.3", "7.2", "7.1"],
         "copy": files_to_copy
     }
