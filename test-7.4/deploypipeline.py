2a6d3f5935ff5401c365dd67d13fbdc381260bb5
Igor Sednev <sednev@raveinfosys.com>
2020-06-02 13:57:29 +0300

    change removeDeployPipeline function

--- test-7.4/deploypipeline.py
+++ test-7.4/deploypipeline.py
@@ -25,8 +25,9 @@ def removeDeployPipeline():
     w.close()
 
 
-def push():
-    subprocess.Popen(['git', 'rm', '--cached', 'bitbucket-pipelines.yml']).communicate()
+def push(url):
+    subprocess.Popen(['git', 'clone', url]).communicate()
+    subprocess.Popen(['cp' '../bitbucket-pipelines.yml']).communicate()
     readFile = open("bitbucket-pipelines.yml")
     lines = readFile.readlines()
     readFile.close()
@@ -34,12 +35,12 @@ def push():
         w = open("bitbucket-pipelines.yml", 'w')
         w.writelines([item for item in lines[:-1]])
         w.close()
-    os.system('echo "#" `date` >> bitbucket-pipelines.yml')
-    subprocess.Popen(['git', 'add', 'bitbucket-pipelines.yml']).communicate()
-    subprocess.Popen(['git', 'commit', '-m', '[skip ci] update pipeline'])
-    os.system('git remote -v')
-    os.system('git fetch --unshallow')
-    os.system('git push --force')
+    subprocess.Popen(['git', 'add', "bitbucket-pipelines.yml"]).communicate()
+    subprocess.Popen(['git', 'commit', '-m' "[SKIP CI] update bitbucket-pipelines.yml"]).communicate()
+    subprocess.Popen(['git', 'push']).communicate()
+    os.system('cd ..')
+
+
 
 os.system('apk add git')
 f = open('repositories')
@@ -52,7 +53,9 @@ for item in repositories:
     if url.find('module') != -1:
         url_list.append(url)
 for url in url_list:
+    os.system('mkdir tmpgit')
     if url.find('module-boilerplate') == -1:
-        process = subprocess.Popen(['git', 'remote', 'set-url', '--add', '--push', 'origin', url]).communicate()
-removeDeployPipeline()
-push()
+        removeDeployPipeline()
+        os.system('cd tmpgit')
+        push(url)
+    os.system('rm -r tmpgit')
